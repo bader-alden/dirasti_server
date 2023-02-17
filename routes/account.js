b14aeec659268e2f907f6ec7e56fed3c03b4f39a
Glitch (ubiquitous-sepia-rainforest) <none>
2023-02-17 18:51:42 +0000

    🚀🎭 Checkpoint ./routes/account.js:51955781/109:52015589/73

--- routes/account.js
+++ routes/account.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var url=require('url');
-
-
 var mysql=require('mysql');
+
 var connection = mysql.createConnection({
         host: 'db4free.net',
         user: 'derasti',
@@ -14,20 +13,20 @@ connection.connect();
 //تسجيل دخول 
 router.post('/login', function(req, res, next) {
     var queryData=url.parse(req.url,true).query;
-    connection.query( "SELECT * FROM users WHERE secret code="+queryData['secret code']+" " ,function(error,results,fields){
+    connection.query( "SELECT * FROM users WHERE secret_code="+queryData['secret_code']+" " ,function(error,results,fields){
         console.log(queryData);
         console.log(error);
         console.log(results);
     if (results!=null){
-    }else  {
-     res.json("notfound >>>>> Contact technical support ");
+    }else{
+     res.json("notfound >>>>> Contact technical support");
        }
     });
     });
   
 router.post('/update_account', function(req, res, next) {
     var queryData=url.parse(req.url,true).query;
-    connection.query("UPDATE users SET email = '"+queryData['email']+"', mobile_id= '"+queryData['mobile_id']+"', is_male= '"+queryData['is_male']+"',grade= '"+queryData['grade']+"' WHERE secret code="+queryData['secret code']+" ",function(error,results,fields){
+    connection.query("UPDATE users SET email = '"+queryData['email']+"', mobile_id= '"+queryData['mobile_id']+"', is_male= '"+queryData['is_male']+"',grade= '"+queryData['grade']+"' WHERE secret_code="+queryData['secret_code']+" ",function(error,results,fields){
         if(!error){
           res.json(true);
         }else{
@@ -35,9 +34,10 @@ router.post('/update_account', function(req, res, next) {
         }
    });
    });
+
 router.post('/delete_account', function(req, res, next) {
     var queryData=url.parse(req.url,true).query;
-    connection.query("DELETE FROM  user  WHERE secret code="+queryData['secret code']+" ",function(error,results,fields){
+    connection.query("DELETE FROM  user  WHERE secret_code="+queryData['secret_code']+" ",function(error,results,fields){
         if(!error){
           res.json(true);
         }else{
@@ -48,15 +48,10 @@ router.post('/delete_account', function(req, res, next) {
 //انشاء حساب 
 router.post('/signin', function(req, res, next) {
     var queryData=url.parse(req.url,true).query;
-    connection.query("INSERT INTO user (`name`, `email`, `is_male`,`grade`,'mobile_id','secret code') VALUES ('"+queryData['name']+"','"+queryData['email']+"','"+queryData['is_male']+"','"+queryData['grade']+"', '"+queryData['mobile_id']+"', '"+queryData['secret code']+"',' ',' ')",function(error,results,fields){
+    connection.query("INSERT INTO user (`name`, `email`, `is_male`,`grade`,'mobile_id','secret_code') VALUES ('"+queryData['name']+"','"+queryData['email']+"','"+queryData['is_male']+"','"+queryData['grade']+"', '"+queryData['mobile_id']+"', '"+queryData['secret_code']+"',' ',' ')",function(error,results,fields){
          console.log(error)
-         console.log(results)
-            
-            });
+         console.log(results) 
+    });
+    });
 
-});
-
-
-//شو هية
-//وين  راحت
 module.exports = router;
