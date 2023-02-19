cb4fbcf84431f318f0ef30cdbd4c334d2473ac99
Glitch (ubiquitous-sepia-rainforest) <none>
2023-02-19 17:40:58 +0000

    🚕👩‍❤️‍💋‍👩 Checkpoint ./routes/copons.js:51955781/479

--- routes/copons.js
+++ routes/copons.js
@@ -11,6 +11,16 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
+router.post('/update_account', function(req, res, next) {
+    var queryData=url.parse(req.url,true).query;
+    connection.query("UPDATE user SET email ='"+queryData['email']+"', mobile_id='"+queryData['mobile_id']+"', is_male= '"+queryData['is_male']+"',grade= '"+queryData['grade']+"' WHERE secret_code="+queryData['secret_code']+" ",function(error,results,fields){
+        if(!error){
+          res.json(true);
+        }else{
+          res.json(false);
+        }
+   });
+   });
 
 
 
