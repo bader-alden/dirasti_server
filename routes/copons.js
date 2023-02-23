271a59c890992cb34a098e1502fd692e7e447fa8
Glitch (ubiquitous-sepia-rainforest) <none>
2023-02-23 09:50:57 +0000

    🚴🐮 Checkpoint ./routes/copons.js:51955781/511:52015589/201

--- routes/copons.js
+++ routes/copons.js
@@ -16,16 +16,16 @@ router.post('/', function(req, res, next) {
     connection.query("SELECT * FROM copon WHERE name_copon='"+queryData['name_copon']+"' ",function(error,results,fields){
       if(!error){
        console.log(results)
-     var json_data = JSON.parse(JSON.stringify(results));         
+     var json_data = JSON.parse(JSON.stringify(results))[0];         
       console.log(json_data['is_open']) 
-      if (json_data['is_open']=='0'){
-    connection.query("INSERT INTO user (`course_file`) VALUES (course_file + "," + '"+queryData['list_cours']+"') WHERE id='"+queryData['id']+"' ",function(error,resullts,fields){
-         console.log(resullts)
+      if (json_data['is_open']=='1'){
+    connection.query("INSERT INTO user (`course_file`) VALUES (CONCAT(course_file ,'"+json_data['list_cours']+"')) WHERE id='"+queryData['id']+"' ",function(error,resullts,fields){
+         console.log(resullts)                                 //شيل هي             //الكوتيشن غلط 
+         console.log(error)//دقيقة لجرب
     });   
-      connection.query("UPDATE copon SET `is_open`='1'  WHERE id='"+json_data['id']+"' ",function(error,resullts,fields){
-       });                               //هي كيف حطا 
-      connection.query("INSERT INTO copon ( `add_by`) VALUES ('"+queryData['id']+"')",function(error,resullts,fields){
-     });
+      connection.query("UPDATE copon SET `is_open`='1' , `add_by`='"+queryData['id']+"' WHERE id='"+json_data['id']+"' ",function(error,resullts,fields){          
+        res.json('تم تفعيل الكورس ');
+      });
      }else{
          res.json('error4');
       }
