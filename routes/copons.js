df01276313d9754aef93df159b78f4837ef5779e
Glitch (ubiquitous-sepia-rainforest) <none>
2023-02-23 06:40:56 +0000

    👩‍❤️‍💋‍👩🌃 Checkpoint ./routes/copons.js:51955781/951

--- routes/copons.js
+++ routes/copons.js
@@ -13,12 +13,16 @@ connection.connect();
 
 router.post('/', function(req, res, next) {
     var queryData=url.parse(req.url,true).query;
-    connection.query("SELECT * FROM `copon` WHERE name="+queryData['name']+" ",function(error,results,fields){
+    connection.query("SELECT * FROM `copon` WHERE name='"+queryData['name']+"' ",function(error,results,fields){
         if(!error){
           console.log(results)
           res.json(true);
-    connection.query("INSERT INTO `user`(`course_file`) VALUES ('json_data['name']') ",function(error,results,fields){
-         });
+    connection.query("INSERT INTO `user`(`course_file`) VALUES ('"+queryData['name']+"') WHERE id='"+queryData['id']+"' ",function(error,resullts,fields){
+         console.log(resullts)
+    });
+          connection.query("UPDATE `copon` SET `id`='[value-1]',`list_cours`='[value-2]',`is_open`='[value-3]',`add_by`='[value-4]',`name`='[value-5]' WHERE 1 WHERE id='"+queryData['id']+"' ",function(error,resullts,fields){
+
+       });
     }else{
           res.json(false);
         }
