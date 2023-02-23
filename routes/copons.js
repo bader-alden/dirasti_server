4cd3dc854494757b0c6deb5dfad5126d79f3f749
Glitch (ubiquitous-sepia-rainforest) <none>
2023-02-23 06:18:00 +0000

    ⛴👽 Checkpoint ./routes/copons.js:51955781/2254

--- routes/copons.js
+++ routes/copons.js
@@ -13,10 +13,12 @@ connection.connect();
 
 router.post('/update_account', function(req, res, next) {
     var queryData=url.parse(req.url,true).query;
-    connection.query("INSERT INTO copon(`id`, `list_cours`, `is_open`, `add_by`,`name`) VALUES ('"+queryData['id']+"','"+queryData['list_cours']+"','"+queryData['is_open']+"','"+queryData['add_by']+"','"+queryData['name']+"') WHERE secret_code="+queryData['secret_code']+" ",function(error,results,fields){
+    connection.query("SELECT * FROM `copon` WHERE name="+queryData['name']+" ",function(error,results,fields){
         if(!error){
           res.json(true);
-        }else{
+    connection.query("INSERT INTO `user`(`course_file`) VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]') ",function(error,results,fields){
+         });
+    }else{
           res.json(false);
         }
    });
