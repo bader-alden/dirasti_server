c5d675ad6679d4780596247163bff48f11fd6aa5
Glitch (ubiquitous-sepia-rainforest) <none>
2023-02-25 18:27:02 +0000

    🌖⛪️ Checkpoint ./routes/ users.js:51955781/54 ./routes/account.js:51955781/4

--- routes/ users.js
+++ routes/ users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  SELECT `id`, `title`, `body` FROM `notices` WHERE 1
   res.send('respond with a resource');
 });
 
--- routes/account.js
+++ routes/account.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var url=require('url');
 var mysql=require('mysql');
-
-
 var connection = mysql.createConnection({
         host: 'db4free.net',
         user: 'derasti',
