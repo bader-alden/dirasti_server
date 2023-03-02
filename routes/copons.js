1881a9cdd745136303cf7cb5ab06fe2f7bf5cfbb
Glitch (ubiquitous-sepia-rainforest) <none>
2023-03-02 01:03:07 +0000

    🏹💎 Checkpoint ./routes/copons.js:52015589/43

--- routes/copons.js
+++ routes/copons.js
@@ -18,12 +18,12 @@ router.post('/', function(req, res, next) {
      var json_data = JSON.parse(JSON.stringify(results))[0];         
       console.log(json_data['is_open']) 
       if (json_data['is_open']=='1'){
-    connection.query("UPDATE user SET course_file = CONCAT(course_file ,'" + "," + json_data['list_cours'] + " ') WHERE id='"+queryData['id']+"' ",function(error,resullts,fields){
+    connection.query("UPDATE user SET course_file = CONCAT(course_file ,'"+"," + json_data['list_cours'] + "') WHERE user_id='"+queryData['user_id']+"' ",function(error,resullts,fields){
          console.log(resullts)                                 //شيل هي              وهدول //الكوتيشن غلط 
          console.log(error)//دقيقة لجرب
     });   
       connection.query("UPDATE copon SET `is_open`='0' , `add_by`='"+queryData['user_id']+"' WHERE id='"+json_data['id']+"' ",function(error,resullts,fields){          
-        res.json('تم تفعيل الكورس ');                    // يس هي 
+        res.json('ok');                  
         console.log(resullts)                         //اشتغل    
          console.log(error)
       });
