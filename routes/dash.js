1b07f25a4a4f739ca065fed43298e5efbf8898e0
Glitch (ubiquitous-sepia-rainforest) <none>
2023-03-14 07:13:24 +0000

    🐺🍱 Checkpoint ./routes/dash.js:51955781/478 ./routes/meddelware.js:51955781/1109

--- routes/dash.js
+++ routes/dash.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var url=require('url');
 var mysql=require('mysql');
+var uploadFile = require ('./middelware.js')
+
 var connection = mysql.createConnection({
         host: 'db4free.net',
         user: 'derasti',
@@ -9,6 +11,24 @@ var connection = mysql.createConnection({
         database: 'derasti'
 });
 connection.connect();
+
+router.post('/uplode' , async (req, res) => {
+  try {
+    await uploadFile(req, res);
+    if (req.file == undefined) {
+      return res.status(400).send({ message: "Please upload a file!" });
+    }
+    res.status(200).send({
+      message: "http://31.220.50.200:3000/file/" + req.file.originalname,
+    });
+    } catch (err) {
+    res.status(500).send({
+      message: `Could not upload the file: "aaa". ${err}`,
+    });
+  }
+});
+
+
 
   router.post('/signin', function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
---
+++ routes/meddelware.js
@@ -0,0 +1,22 @@
+const util = require("util");
+const multer = require("multer");
+const maxSize = 5 * 1024 * 1024;
+global.__basedir = __dirname;
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null,   "./public/file/");
+  },
+  filename: (req, file, cb) => {
+    console.log(file.originalname);
+    cb(null, file.originalname);
+  },
+});
+
+let uploadFile = multer({
+  storage: storage,
+  limits: { fileSize: maxSize },
+}).single("file");
+
+let uploadFileMiddleware = util.promisify(uploadFile);
+module.exports = uploadFileMiddleware;
