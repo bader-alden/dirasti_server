0713c97e50dbf38409b615b13255cda285f950a8
Glitch (ubiquitous-sepia-rainforest) <none>
2023-03-15 20:39:14 +0000

    🐀🍙 Checkpoint ./routes/dash.js:51955781/296:52015589/46

--- routes/dash.js
+++ routes/dash.js
@@ -12,10 +12,10 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
-const a = function (req, res, next) {
+const admin= function (req, res, next) {
   //res.send('esraa  developed this servar and her best help her >>>  ')
   var queryData=url.parse(req.url,true).query; 
-connection.query( "SELECT is_read FROM dashboard WHERE id= '"+queryData['id']+"' AND password= '"+queryData['pass']+"'" ,function(error,results,fields){
+connection.query( "SELECT is_read FROM dashboard WHERE id= '"+queryData['id']+"' AND password= '"+queryData['password']+"'" ,function(error,results,fields){
    console.log(error)
   console.log(results)
      var json_data = JSON.parse(JSON.stringify(results))[0];
@@ -28,22 +28,15 @@ if(json_data['is_read']==0){
   res.send('esraa  developed this servar and her best help her >>>  ')
 }
 });
-//we are forget
-
-// ok ok 
-
-
-
-
-
 }
 
 
-router.get('/test' , a , function(req, res, next) {
-      res.json("results");
+// router.get('/test' , admin , function(req, res, next) {
+  
+//       res.json("results");
 
-   });
-router.post('/signin', function(req, res, next) {
+//    });
+router.post('/signin',  function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
   connection.query( "SELECT * FROM dashboard WHERE name= '"+queryData['name']+"' AND password= '"+queryData['password']+"'" ,function(error,results,fields){
       console.log(results);
@@ -62,7 +55,7 @@ var queryData=url.parse(req.url,true).query;
    });
 
 
-router.post('/user', function(req, res, next) {
+router.post('/user',admin , function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
   connection.query("DELETE from user WHERE id='"+queryData['id']+"'" ,function(error,results,fields){
     if(!error){
@@ -88,7 +81,7 @@ var queryData=url.parse(req.url,true).query;
 
 router.post('/signin', function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
-  connection.query( "SELECT * FROM dashboard WHERE user= '"+queryData['user']+"' AND pass= '"+queryData['pass']+"'" ,function(error,results,fields){
+  connection.query( "SELECT * FROM dashboard WHERE user= '"+queryData['user']+"' AND password= '"+queryData['password']+"'" ,function(error,results,fields){
       console.log(results);
       res.json(results);
   });
@@ -106,10 +99,10 @@ var queryData=url.parse(req.url,true).query;
 
 router.post('/change_pass', function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
-  connection.query( "SELECT * FROM dashboard WHERE id= '"+queryData['id']+"' AND pass= '"+queryData['pass']+"'" ,function(error,results,fields){
+  connection.query( "SELECT * FROM dashboard WHERE id= '"+queryData['id']+"' AND password= '"+queryData['password']+"'" ,function(error,results,fields){
       console.log(results);
      if(results.length >0){
-        connection.query("UPDATE dashboard SET pass = '"+queryData['new_pass']+"' WHERE id='"+queryData['id']+"'",function(error,results,fields){
+        connection.query("UPDATE dashboard SET password= '"+queryData['new_password']+"' WHERE id='"+queryData['id']+"'",function(error,results,fields){
           if(!error){
             res.send("yes")
           }
@@ -143,7 +136,7 @@ var queryData=url.parse(req.url,true).query;
     }
   });
 });
-router.post('/insert', function(req, res, next) {
+router.post('/insert',admin, function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
  connection.query("INSERT INTO "+queryData['table']+" ("+queryData['sql_key']+") VALUES ("+queryData['sql_value']+")  ",function(error,results,fields){
     if(!error){
@@ -155,7 +148,7 @@ var queryData=url.parse(req.url,true).query;
   });
 });
 
-router.post('/insert_id', function(req, res, next) {
+router.post('/insert_id', admin,function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
  connection.query("INSERT INTO "+queryData['table']+" ("+queryData['sql_key']+") VALUES ("+queryData['sql_value']+") WHERE id='"+queryData['id']+"'",function(error,results,fields){
     if(!error){
@@ -166,7 +159,7 @@ var queryData=url.parse(req.url,true).query;
     }
   });
 });
-router.post('/update_id', function(req, res, next) {
+router.post('/update_id',admin, function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
  connection.query("UPDATE "+queryData['table']+" SET "+queryData['sql_key']+" WHERE id='"+queryData['id']+"'",function(error,results,fields){
     if(!error){
@@ -177,7 +170,7 @@ var queryData=url.parse(req.url,true).query;
     }
   });
 });
-router.post('/update_id_user', function(req, res, next) {
+router.post('/update_id_user',admin, function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
  connection.query("UPDATE "+queryData['table']+" SET "+queryData['sql_key']+" WHERE user_id='"+queryData['id']+"'",function(error,results,fields){
     if(!error){
@@ -188,7 +181,7 @@ var queryData=url.parse(req.url,true).query;
     }
   });
 });
-router.post('/delet_id', function(req, res, next) {
+router.post('/delet_id',admin, function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
   connection.query("DELETE  from "+queryData['table']+" WHERE id='"+queryData['id']+"'" ,function(error,results,fields){
     if(!error){
@@ -199,7 +192,7 @@ var queryData=url.parse(req.url,true).query;
     }
   });
 });
-router.post('/delet_id_user', function(req, res, next) {
+router.post('/delet_id_user',admin, function(req, res, next) {
 var queryData=url.parse(req.url,true).query; 
   connection.query("DELETE  from "+queryData['table']+" WHERE user_id='"+queryData['id']+"'" ,function(error,results,fields){
     if(!error){
