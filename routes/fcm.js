7ccb95a08e5dfe2d912207df2c4d3958b10c8856
Glitch (ubiquitous-sepia-rainforest) <none>
2023-03-15 09:07:42 +0000

    📠⛷ Checkpoint ./routes/fcm.js:51955781/292:52015589/267

--- routes/fcm.js
+++ routes/fcm.js
@@ -24,17 +24,7 @@ connection.connect();
 //const registrationToken = 'cijAcgXuTHmYhsDn89XvJ2:APA91bHleFKQHPCxuB0KSZnNdmV7x9eE0OmwutScoV7yo4qtOB8yj1GuNUNAMTMX5ZbjkQzi7oXicO_pCKZ6UDJhV2ii3UrDuko6wdsezMe7gugqSzHLK5QpHyfgtLDbubdUcGXv0G0f';
 router.get('/all', function(req, res, next) {
 var queryData=url.parse(req.url,true).query;
- if(queryData[public'){
-   var topic = "/topics/all";
-   //      /topics/all
- }
-  if(results != "" &&results != " " &&results != "[]" &&results != undefined&&results != null) 
-  {
-    var json_data = JSON.parse(JSON.stringify(results))[0]
-    var t = json_data['gsm_token']+":"+json_data['gsm_token2'];  //هاد السطر صح
-    }
- if(t !=null)
- { const message = {
+  const message = {
      'android': {
       "notification": {
         "title":  "دراستي",
@@ -44,18 +34,15 @@ var queryData=url.parse(req.url,true).query;
         "priority": "high",
         "icon": "myicon",
         "channelId": "high_importance_channel",
-      }}, 
-//   token: t ?? "",
-   // topic:topic ?? ""
-   
-    //        /topics/all
+      }},
+       "topic" : "/topics/all"
 };
  admin.messaging().send(message)
   .then((response) => {
    res.json(response)
    console.log('Successfully sent message:', response);
    var queryDat=url.parse(req.url,true).query;
-   connection.query("INSERT INTO notices (`title`, `body`, `user_id`) VALUES ('"+queryDat['title']+"','"+queryDat['body']+"','"+queryDat['user_id']+"')", function(error,results,fields){
+   connection.query("INSERT INTO notices (`title`, `body`, `user_id`) VALUES ('"+queryDat['title']+"','"+queryDat['body']+"','public')", function(error,results,fields){
    console.log(error)
    console.log(results) 
    });
@@ -66,10 +53,9 @@ var queryData=url.parse(req.url,true).query;
       res.json(error)
     console.log('Error sending message:', error);
   });
- }
+ 
   
 });
-  });
 
 
 router.get('/res_one', function(req, res, next) {
