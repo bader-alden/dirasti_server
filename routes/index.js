54d1c8ace1e86b1db57fdadf2a5cbcc629d2bf92
Glitch (ubiquitous-sepia-rainforest) <none>
2023-03-16 09:15:35 +0000

    🏅🌴 Checkpoint ./routes/index.js:51955781/51

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,6 @@ router.get('/version', function(req, res, next) {
    var queryData=url.parse(req.url,true).query;
  connection.query("SELECT `version` FROM version WHERE id ='1'",function(error,resullt,fields){ 
   console.log(resullt);
-
    }); 
    }); 
 
