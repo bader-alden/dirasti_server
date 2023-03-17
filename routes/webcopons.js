bf1a19fb77c3ea664c653790596c1dfba9fd9b19
Glitch (ubiquitous-sepia-rainforest) <none>
2023-03-17 08:38:27 +0000

    👕👱 Checkpoint ./routes/webcopons.js:51955781/5391

--- routes/webcopons.js
+++ routes/webcopons.js
@@ -12,9 +12,51 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-router.get('/version', function(req, res, next) {
+router.get('/', function(req, res, next) {
    var queryData=url.parse(req.url,true).query;
- connection.query("SELECT `version` FROM version WHERE id ='1'",function(error,resullt,fields){ 
-  console.log(resullt);
+ connection.query("SELECT `id`, `list_cours`, `is_open`, `add_by`, `name_copon`, `uid_copon`, `price`, `grade` FROM copon  ",function(error,resullt,fields){ 
+  var json_data = JSON.parse(JSON.stringify(resullt))[0]['list_cours'];
+   console.log(resullt);
+   if(check_course(t,json_data)){
+      console.log(json_data)
+      res.send('error1');
+    }else { 
+   if (json_datta==json_dat){
+        console.log(resullt)
+         res.send(resullt)
+     }else{
+        res.send('error10')
+    }
+    }
+   });
+   }else{
+  res.send("notfound") 
+}
+   });         
+});
+
+function check_file(list , queryData){
+  for(var i=0 ;i<list.length ; i++){
+      console.log(list);
+       var m=list[i].split("|")
+     if(m[0]==queryData['is_course'] &&
+         m[1]==queryData['grade'] && 
+         m[2]==queryData['subject'] && 
+         m[3]==queryData['teacher_name'] && 
+         m[4]==queryData['file']
+       ){
+       return true;
+       }
+     }
+  return false;
+}
+
+  
    }); 
    }); 
+
+
+
+
+
+module.exports = router;
